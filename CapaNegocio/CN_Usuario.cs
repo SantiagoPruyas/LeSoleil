34bb6f8f296f4d9d2e0b31a276a82f4d8df8ff36
Santiago Pruyas <spruyas@hotmail.com>
2024-09-30 12:25:59 -0300

    Conexiones entre capas y creacion de CN_Usuario

---
+++ CapaNegocio/CN_Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Usuario
+    {
+
+        private CD_usuario objcd_usuario = new CD_usuario();
+
+        public List<Usuario> Listar()
+        {
+            return objcd_usuario.Listar();
+        }
+
+    }
+}
